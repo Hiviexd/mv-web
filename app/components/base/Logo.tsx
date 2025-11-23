899e57d5f91784aa2705d8be9a1f512ba9e5a990
Hivie <me@hivie.tn>
2025-11-23 22:01:01 +0100

    support logo glow param

--- app/components/base/Logo.tsx
+++ app/components/base/Logo.tsx
@@ -1,10 +1,16 @@
+import { Box } from "@mantine/core";
 import logo from "../../assets/logo.svg";
 
 interface LogoProps {
     width?: number | string;
     height?: number | string;
+    glow?: boolean;
 }
 
-export function Logo({ width = 40, height = 40 }: LogoProps) {
-    return <img src={logo} alt="Mapset Verifier" width={width} height={height} style={{ display: "block" }} />;
+export function Logo({ width = 40, height = 40, glow = false }: LogoProps) {
+    return (
+        <Box mod={[{ glow: glow }]} className="logo">
+            <img src={logo} alt="Mapset Verifier" width={width} height={height} style={{ display: "block" }} />
+        </Box>
+    );
 }
