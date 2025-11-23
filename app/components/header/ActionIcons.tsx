cdab0f250a2c2a680b720e2e6ea83b8d479dc580
Hivie <me@hivie.tn>
2025-11-23 23:10:45 +0100

    add bug report button

--- app/components/header/ActionIcons.tsx
+++ app/components/header/ActionIcons.tsx
@@ -1,10 +1,22 @@
-import { Button, Group } from "@mantine/core";
-import { IconBrandGithub } from "@tabler/icons-react";
+import { ActionIcon, Button, Group, Tooltip } from "@mantine/core";
+import { IconBrandGithub, IconBug } from "@tabler/icons-react";
 import metadata from "../../data/metadata.json";
 
 export function ActionIcons() {
     return (
         <Group gap="sm">
+            <Tooltip label="Report Issues">
+                <ActionIcon
+                    size="lg"
+                    component="a"
+                    href={metadata.repository + "/issues"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variant="light"
+                    color="primary.2">
+                    <IconBug size={20} />
+                </ActionIcon>
+            </Tooltip>
             <Button
                 component="a"
                 href={metadata.repository}
